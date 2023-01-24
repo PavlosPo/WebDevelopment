c3a718517e9891637fa11e3564af1b41d2322bc6
PavlosPo <paulospoulos7@gmail.com>
2023-01-24 14:46:01 +0200

    Implement flask, Jinja Template Language
    Just to make the webpage more dynamic, included a lot of include statements (Jinja) as well as for loops in the HTML e.t.c.

--- Blog-Capstone-Project-Part2/main.py
+++ Blog-Capstone-Project-Part2/main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, url_for
-
+import requests
+import datetime
 app = Flask(__name__)
+api_endpoint = "https://api.npoint.io/f8151ab1f10f7e9cf7a8"
 
+
+def api_brain():
+    response = requests.get(url=api_endpoint)
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    data = api_brain()
+    current_date = datetime.datetime.now()
+    return render_template("index.html",
+                           data=data,
+                           month=current_date.strftime("%B"),
+                           day_of_date=current_date.day,
+                           year_of_date=current_date.year)
 
 
 @app.route("/about")
