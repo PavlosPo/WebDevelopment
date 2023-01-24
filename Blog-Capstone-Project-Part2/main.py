afd232047739ee80bab04d882803bc2a66a77cbe
PavlosPo <paulospoulos7@gmail.com>
2023-01-24 15:43:00 +0200

    Completed the Project Blog 2
    Completed the Blog Project Part 2.
    Used Headers, footers, navigation bar external files, as well as Flask implemantation and Jinja Template Language to work efficiently with clean code.

--- Blog-Capstone-Project-Part2/main.py
+++ Blog-Capstone-Project-Part2/main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, url_for
 import requests
 import datetime
+
 app = Flask(__name__)
 api_endpoint = "https://api.npoint.io/f8151ab1f10f7e9cf7a8"
 
 
-def api_brain():
-    response = requests.get(url=api_endpoint)
-    response.raise_for_status()
-    data = response.json()
+def api_brain(id=None):
+    if id is None:
+        # Get all the data
+        response = requests.get(url=api_endpoint)
+        response.raise_for_status()
+        data = response.json()
+    else:
+        # Get only the data with the specific id
+        data = api_brain()
+        specific_post = [post for post in data if int(post['id']) == int(id)][0]
+        return specific_post
     return data
+
 
 @app.route("/")
 def home_page():
@@ -34,8 +43,14 @@ def contact_page():
 
 @app.route("/post/<id>")
 def create_post_page(id):
+    current_post_data = api_brain(id=id)
+    current_date = datetime.datetime.now()
     return render_template("post.html",
-                           post_id=id)
+                           post_id=id,
+                           current_post=current_post_data,
+                           month=current_date.strftime("%B"),
+                           day_of_date=current_date.day,
+                           year_of_date=current_date.year)
 
 
 if __name__ == "__main__":
