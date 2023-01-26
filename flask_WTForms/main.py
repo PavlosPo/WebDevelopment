1a02109e70f74386fa5b5d07e873df9f9e65d028
PavlosPo <paulospoulos7@gmail.com>
2023-01-26 15:24:14 +0200

    Created csrf fork protection
    Also added error displaying when the user inputs invalid info

--- flask_WTForms/main.py
+++ flask_WTForms/main.py
@@ -1,33 +1,35 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
 
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = '5f18d396d4038e7b684543b2f2b79af4'
+app.secret_key = '5f18d396d4038e7b684543b2f2b79af4'
 
 
 # Class for Flask WTForms
 class Form(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[Email()])
-    message = TextAreaField('message', validators=[DataRequired()])
-    send = SubmitField('send')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Login")
 
 
 @app.route("/")
 def home():
-    # Create the Form Class and form object to pass it into the HTML file
-    form = Form()
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        return render_template('login.html')
+    # Create the Form Class and form object to pass it into the HTML file
+    login_form = Form()
+
+    # If we have a POST Request or else the form is making a submit then print data
+    if login_form.validate_on_submit():
+        print(login_form.data)
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
