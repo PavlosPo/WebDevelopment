53c90032446b418d180253d90e7f26111d6e770e
PavlosPo <paulospoulos7@gmail.com>
2023-01-26 17:11:36 +0200

    Used some Jinja Inheritance Block Template HTML Code

--- flask_WTForms/main.py
+++ flask_WTForms/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 app = Flask(__name__)
@@ -11,8 +11,10 @@ app.secret_key = '5f18d396d4038e7b684543b2f2b79af4'
 
 # Class for Flask WTForms
 class Form(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired(), Email()])
-    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    email = StringField(label='Email', validators=[DataRequired(),
+                                                   Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(),
+                                                           Length(min=8)])
     submit = SubmitField(label="Login")
 
 
@@ -27,8 +29,14 @@ def login():
     login_form = Form()
 
     # If we have a POST Request or else the form is making a submit then print data
-    if login_form.validate_on_submit():
-        print(login_form.data)
+    if login_form.validate_on_submit():  # If submited AND validated AND Credentials match then True
+        correct_email = "admin@email.com"
+        correct_password = "12345678"
+        # If correct email credentials
+        if login_form.data['email'] == correct_email and login_form.data['password'] == correct_password:
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
