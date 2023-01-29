4315b1813666ec1f24831cd7e6de8aec2e6a2d53
PavlosPo <paulospoulos7@gmail.com>
2023-01-29 15:51:56 +0200

    Using SQLite using Flask-SQLAlchemy

--- Library_Start/main.py
+++ Library_Start/main.py
@@ -1,14 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+
+    with app.app_context():
+        init_db()
+
+    return app
+
+app = create_app()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+db = SQLAlchemy(app)
 all_books = []
 
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()  # It modifies, creates, interact with the files
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()  # It modifies, creates, interact with the files
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+class Library(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False, unique=True)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+    def __init__(self, id, title, author, rating):
+        db.drop_all()
+        self.id = id
+        self.title = title
+        self.author = author
+        self.rating = rating
+        db.create_all()
+
 
 
 @app.route('/')
@@ -17,6 +47,9 @@ def home():
         empty_list_of_books = True
     else:
         empty_list_of_books = False
+    test_data = Library('Hary Potter', 'J. K. Rowling', 9.3)
+    db.session.add(test_data)
+    db.session.commit()
     return render_template('index.html',
                            all_books=all_books,
                            empty_list_of_books=empty_list_of_books)
@@ -34,5 +67,7 @@ def add():
 
 
 if __name__ == "__main__":
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
 
