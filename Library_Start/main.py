c6c45ae343e3c17aed6f2e631381769abdafb90d
PavlosPo <paulospoulos7@gmail.com>
2023-01-31 15:21:47 +0200

    Created a databsase using Flask-SQLAlchemy

--- Library_Start/main.py
+++ Library_Start/main.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
 import sqlite3
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-def create_app():
-    app = Flask(__name__)
-
-    with app.app_context():
-        init_db()
-
-    return app
-
-app = create_app()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
 db = SQLAlchemy(app)
-all_books = []
-
 
 # db = sqlite3.connect("books-collection.db")
 # cursor = db.cursor()  # It modifies, creates, interact with the files
@@ -25,31 +19,42 @@ all_books = []
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
-class Library(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), nullable=False, unique=True)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float(), nullable=False)
+with app.app_context():
+    class Book(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(250), nullable=False, unique=True)
+        author = db.Column(db.String(250), nullable=False)
+        rating = db.Column(db.Float(), nullable=False)
 
-    def __init__(self, id, title, author, rating):
-        db.drop_all()
-        self.id = id
-        self.title = title
-        self.author = author
-        self.rating = rating
-        db.create_all()
+        def __init__(self, title, author, rating):
+            self.title = title
+            self.author = author
+            self.rating = rating
+
+        def __repr__(self):
+            return '<User %r>' % self.title
+
+    # db.create_all()
+    # new_book = Book(id=2, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    # db.session.add(new_book)
+    # db.session.commit()
 
 
+class BooksForm(FlaskForm):
+    title = StringField("Book's Name", validators=[DataRequired()])
+    author = StringField("Book's Author", validators=[DataRequired()])
+    rating = StringField("Book's Rating", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
+
+# Flask Section
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     if len(all_books) == 0:
         empty_list_of_books = True
     else:
         empty_list_of_books = False
-    test_data = Library('Hary Potter', 'J. K. Rowling', 9.3)
-    db.session.add(test_data)
-    db.session.commit()
     return render_template('index.html',
                            all_books=all_books,
                            empty_list_of_books=empty_list_of_books)
@@ -58,16 +63,27 @@ def home():
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        current_book_dictionary = {'title': request.form.get('book'), 'author': request.form.get('book_author'),
-                                   'rating': request.form.get('rating')}
-        all_books.append(current_book_dictionary)
+        # current_book_dictionary = {'title': request.form.get('book'), 'author': request.form.get('book_author'),
+        #                            'rating': request.form.get('rating')}
+        book_to_add = Book(title=request.form.get('book'),
+                           author=request.form.get('book_author'),
+                           rating=request.form.get('rating'))
+        db.session.add(book_to_add)
+        db.session.commit()
         return redirect(url_for('home'))
     else:
         return render_template('add.html')
 
 
-if __name__ == "__main__":
-    db.drop_all()
-    db.create_all()
-    app.run(debug=True)
+@app.route('/change_rating/<id>')
+def change_rating(id):
+    print(id)
+    current_book = db.session.get(id=id)
+    new_rating = None
+    current_book.rating = new_rating
+    print(current_book)
+    return redirect(url_for('home'))
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
