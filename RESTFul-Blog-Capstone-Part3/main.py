31bf6aa99390628c26ddf720091f1b932ec5b7e4
PavlosPo <paulospoulos7@gmail.com>
2023-02-04 15:39:14 +0200

    Updated the Project so we can delete Posts from database

--- RESTFul-Blog-Capstone-Part3/main.py
+++ RESTFul-Blog-Capstone-Part3/main.py
@@ -79,6 +79,14 @@ def new_post():
     return render_template('make-post.html', form=form)
 
 
+@app.route("/delete/<post_id>")
+def delete_post(post_id):
+    post_to_delete = db.session.query(BlogPost).get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
